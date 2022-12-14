3b5a79b56107148c2f710551a46b15fe0b3fb1ec
domrigby <domgdrigby@gmail.com>
2022-12-14 17:14:27 +0000

    couple changes

--- main.py
+++ main.py
@@ -1,59 +1,14 @@
 
 import numpy as np
-import numpy.linalg as la
-
 import pygame
 
-global G 
-G = 6.67430e-11
-
-class planet():
-
-    def __init__(self,ID,pos,vel,mass):
-
-        self.ID = ID
-
-        self.pos = np.array(pos)
-        self.vel = np.array(vel)
-        self.mass = mass
-
-        self.posList = []
-
-
-    def updateState(self,deltaT,planetList):
-
-        force = np.array([0,0])
-        
-        for planet in planetList:
-            if self.ID != planet.ID:
-                r = planet.pos - self.pos
-
-                rUnit = r/la.norm(r)
-
-                forceVec = (G*self.mass*planet.mass/la.norm(r)**2)*rUnit
-
-                force = np.add(force,forceVec)
-
-        self.acc = force/self.mass
-
-        self.pos = self.pos + np.multiply(self.vel,deltaT) + np.multiply(self.acc,deltaT**(2)/2)
-        self.vel = self.vel + np.multiply(self.acc,deltaT)
-
-
-
-    def savePos(self,pos):
-
-        if pos != self.posList[-1]:
-            self.posList.append(pos)
-
-
+from planet import planet
 
 def posToPixel(pos,xHeight,yWidth,pixHeight,pixWidth):
     pixPosX = int((pos[0]/xHeight)*pixHeight)
     pixPosY = int((pos[1]/yWidth*pixWidth))
 
     return np.array([pixPosX,pixPosY])
-
 
 def main():
 
